c5f8f0c6d762cc726de0973e87e8e23e858d64ac
Joseph <planetapluton888@gmail.com>
2025-02-01 16:58:06 +0600

    Update README.MD

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
 plugins {
-    alias(libs.plugins.kotlin.multiplatform).apply(false)
-    alias(libs.plugins.compose.compiler).apply(false)
-    alias(libs.plugins.compose.multiplatform).apply(false)
+    alias(libs.plugins.binary.compat)
 }
